8261901918a11307c7d082566cfe69acc556f054
Ana Carolina Fonseca <ana.fonseca27@escola.pr.gov.br>
2025-09-30 07:58:26 -0300

    b

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = Document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    avanca.forEach(button => {
-        button.addEvenListener('click',function(){
-const atual = document.querySelector('ativo');
-const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-atual.classList.remove('ativo');
-document.getElementById(proximoPasso).classList.add('ativo');
-        })
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
+})
